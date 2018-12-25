7339f02f153f8b5acf3f17ac5c418c2faf2a3d59
ingleonelrv <ing.leonelrv@gmail.com>
2018-12-24 18:34:55 -0600

    separador y lista vacia?

--- src/videos/containers/suggestion-list.js
+++ src/videos/containers/suggestion-list.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
 import {FlatList, Text} from 'react-native'
 import SuggestionListLayout from '../components/suggestion-list-layout'
+import Empty from '../components/empty'
+import VerticalSeparator from '../components/vertical-separator'
 
 class SuggestionList extends Component{
+    renderEmpty = () => <Empty text='No hay sugerencias en la lista' />
+    itemSeparator = () => <VerticalSeparator color='red' />
     render(){
         //temporalmente creo un array de datos para la lista
+        // const lista = []
         const list = [
             {
                 title: 'Titulo A',
@@ -19,6 +24,8 @@ class SuggestionList extends Component{
             <SuggestionListLayout title='Sugerencias para ti'>
                 <FlatList 
                     data={list}
+                    ListEmptyComponent={this.renderEmpty}
+                    ItemSeparatorComponent={this.itemSeparator}
                     renderItem={({item})=> <Text>{item.title}</Text>}
                 />
             </SuggestionListLayout>
