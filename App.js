547f73270744e1684343a7c86c3b3c80a1ed9997
ingleonelrv <ing.leonelrv@gmail.com>
2018-12-26 11:22:17 -0600

    haciendo peticiones a un servidor con fetch

--- App.js
+++ App.js
@@ -4,9 +4,14 @@ import {Text} from 'react-native';
 import Home from './src/screens/containers/home'
 import Header from './src/sections/components/header'
 import SuggestionList from './src/videos/containers/suggestion-list'
+import API from './utils/util'
 
 type Props = {};
 export default class App extends Component<Props> {
+  async componentDidMount(){
+    const movies = await API.getSuggestions(10);
+    console.log(movies);
+  }
   render() {
     return (
       <Home>
---
+++ utils/util.js
@@ -0,0 +1,13 @@
+const BASE_API = 'https://yts.am/api/v2/'
+
+class Api{
+    async getSuggestions(id){
+        //fetch retorna una promesa
+        const query = await fetch(`${BASE_API}movie_suggestions.json?movie_id=${id}`)
+        //.json() tambien retorna una promesa por eso tambien lleva await
+        const { data } = await query.json()
+        return data.movies
+    }
+}
+//exporto una instancia de la clase
+export default new Api
