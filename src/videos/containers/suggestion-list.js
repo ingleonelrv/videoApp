50028e1ebebb434d017b540355ae938fa91987db
ingleonelrv <ing.leonelrv@gmail.com>
2018-12-26 12:11:10 -0600

    Diseniando componente Suggestions

--- src/videos/containers/suggestion-list.js
+++ src/videos/containers/suggestion-list.js
@@ -3,10 +3,16 @@ import {FlatList, Text} from 'react-native'
 import SuggestionListLayout from '../components/suggestion-list-layout'
 import Empty from '../components/empty'
 import VerticalSeparator from '../components/vertical-separator'
+import Suggestion from '../components/suggestions'
 
 class SuggestionList extends Component{
     renderEmpty = () => <Empty text='No hay sugerencias en la lista' />
     itemSeparator = () => <VerticalSeparator color='red' />
+    renderItem = ({item}) =>{
+        return(
+            <Suggestion {...item} />
+        )
+    }
     render(){
         //temporalmente creo un array de datos para la lista
         // const lista = []
@@ -26,7 +32,7 @@ class SuggestionList extends Component{
                     data={list}
                     ListEmptyComponent={this.renderEmpty}
                     ItemSeparatorComponent={this.itemSeparator}
-                    renderItem={({item})=> <Text>{item.title}</Text>}
+                    renderItem={this.renderItem}
                 />
             </SuggestionListLayout>
         )
